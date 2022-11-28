713f85d24b82c46616cca36c19976297adb795d7
Z-kamili <zakariakamili97@gmail.com>
2022-11-28 17:30:00 +0000

    new promises

--- promise.js
+++ promise.js
@@ -1,11 +1,13 @@
 const p = new Promise((resolve,reject)=>{
-
     //Kick off some async work
     //...
-    resolve(1);
+    setTimeout(() => {
+        resolve(1); // pending =>  resolved, fulfilled
+        reject(new Error('message')); // pending => rejected
+    }, 2000);
     // reject(new Error('message'));
-
-
 });
 
-p.then(result => console.log('Result',result));
+p
+ .then(result => console.log('Result',result))
+ .catch(err => console.log('Error',err.message));
