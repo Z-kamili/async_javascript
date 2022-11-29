396dac1404e95dc08865c2eb460e7132faf70229
Z-kamili <zakariakamili97@gmail.com>
2022-11-29 07:21:16 +0000

    more code

--- promise-api.js
+++ promise-api.js
@@ -1,2 +1,23 @@
-const p = Promise.reject(new Error('reason for rejection...'));
-p.then(error => console.log(error));
+// const p = Promise.reject(new Error('reason for rejection...'));
+// p.then(error => console.log(error));
+
+const p1 = new Promise((resolve) => {
+
+     setTimeout(()=> {
+        console.log('Async operation 1...');
+        resolve(1);
+     },2000);
+
+});
+
+const p2 = new Promise((resolve) => {
+
+    setTimeout(()=> {
+       console.log('Async operation 1...');
+       resolve(1);
+    },2000);
+
+});
+
+Promise.all([p1,p2])
+       .then(result => console.log(result));
